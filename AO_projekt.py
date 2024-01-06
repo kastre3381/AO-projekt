089376176e0eee2b854c7f2995bb0d03c02aec6d
Jakub Mikusek <j.mikusek01@gmail.com>
2024-01-06 14:20:22 +0100

    Encapsulated GUI to a class and moved OCR alg to backgroung thread

---
+++ AO_projekt.py
@@ -0,0 +1,83 @@
+from Detection import *
+
+import os
+from tkinter import *
+from tkinter import ttk
+from tkinter import filedialog
+from threading import *
+
+class GUI:
+	def __init__(self, context):
+		self.root = context
+		self.root.title("Licence Plate Recognition")
+		self.root.resizable(False, False)
+		self.root.geometry("800x600")
+
+	def CreateMainFrame(self) :
+		self.mainframe = ttk.Frame(self.root)
+		self.mainframe.grid()
+	
+	def CreateImageFrame(self):
+		self.imgframe = ttk.Frame(self.mainframe, borderwidth=1, relief="groove", width=800, height=480)
+		self.imgframe.grid(column=0, row=0)
+		self.imgframe.grid_rowconfigure(0, weight=1)
+		self.imgframe.grid_columnconfigure(0, weight=1)
+		self.imgframe.grid_propagate(False)
+		self.loaded_image = ttk.Label(self.imgframe)
+
+	def CreateMenuFrame(self):
+		self.menuframe = ttk.Frame(self.mainframe, borderwidth=1, relief="groove", width=800, height=100, padding="3 3 3 3")
+		self.menuframe.grid(column=0, row=2)
+		self.menuframe.grid_columnconfigure(1, weight=1)
+		self.menuframe.grid_propagate(False)
+
+		Button(self.menuframe, text="Load image", height=7, command=self.browseFiles).grid(column=0, row=0, rowspan = 2)
+		ttk.Label(self.menuframe, text='Recognised plate number:').grid(column=1, row=0)
+		self.recognised_number = ttk.Label(self.menuframe)
+
+	def CreateProgressBar(self) :
+		self.spacing = ttk.Frame(self.mainframe, width=800, height=20)
+		self.spacing.grid(column=0, row=1)
+		self.progressBar = ttk.Progressbar(self.spacing, orient='horizontal', length=800, mode='determinate')
+		self.progressBar.grid(column=0, row=0)
+	
+	def LoadImage(self, filepath : str):
+		self.image = PhotoImage(file=filepath, master=self.root)
+		self.loaded_image.configure(image=self.image)
+		self.loaded_image.grid(column=0, row=0)
+		self.loaded_image.grid_columnconfigure(0, weight=1)
+	
+	def PlateToText(self, filename : str):
+		self.recognisedNumberText = PlateDetection(filename, Method1, self.progressBar)
+		if len(self.recognisedNumberText) == 0 :
+			self.recognisedNumberText = PlateDetection(filename, Method2, self.progressBar)
+		if len(self.recognisedNumberText) == 0 :
+			self.recognisedNumberText = PlateDetection(filename, Method3, self.progressBar)
+		if len(self.recognisedNumberText) == 0 :
+			self.recognisedNumberText = "nie znaleziono"
+		self.progressBar['value'] = 100
+
+
+	def LoadPlateNumber(self, filename : str) :
+		self.PlateToText(filename)
+		self.recognised_number.configure(text = self.recognisedNumberText)
+		self.recognised_number.grid(column=1, row=1)
+		# self.progressBar.stop()
+
+	def browseFiles(self):
+		filename = filedialog.askopenfilename(initialdir = os.getcwd(),
+										title = "Select an Image",
+										filetypes = [("Images", "*.png")])
+		self.progressBar['value'] = 0
+		self.t1 = Thread(target=lambda : self.LoadPlateNumber(filename))
+		self.t1.start()
+		self.recognisedNumberText = ""
+		self.LoadImage(filename)
+
+root = Tk()
+context = GUI(root)
+context.CreateMainFrame()
+context.CreateProgressBar()
+context.CreateImageFrame()
+context.CreateMenuFrame()
+root.mainloop()
---
+++ Detection.py
@@ -0,0 +1,62 @@
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+import imutils
+import easyocr
+
+def TextDetection(platepath : str) :
+	reader = easyocr.Reader(['en'])
+	img = cv2.imread(platepath)
+	results = reader.readtext(img)
+	plate_number = ""
+	for res in results:
+		letter = res[1]
+		plate_number += letter
+	return plate_number
+
+def ObjectFinder(bin_img, real_img) :
+	objects_list = cv2.findContours(bin_img, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+	parents_idx_list = [objects_list[1][0][i][3] for i in range(0, len(objects_list[1][0])) if objects_list[1][0][i][3] != -1]
+	parents_idx_list = np.unique(np.array(parents_idx_list)).tolist()
+	counter = 0
+	path_list = []
+	for idx in parents_idx_list :
+		perimeter = cv2.arcLength(objects_list[0][idx], True)
+		polygon = cv2.approxPolyDP(objects_list[0][idx], 10, True)
+		if len(polygon) == 4 :
+			mask1 = np.zeros(bin_img.shape, np.uint8)
+			cv2.fillConvexPoly(mask1, objects_list[0][idx], 255)
+			x, y, w, h = cv2.boundingRect(objects_list[0][idx])
+			cropped = cv2.bitwise_and(real_img, real_img, mask = mask1)[y:y+h, x:x+w]
+			cv2.imwrite("cropped_results/" + str(counter) + ".png", cropped)
+			path_list.append("cropped_results/" + str(counter) + ".png")
+			counter += 1
+	return path_list
+
+def Method1(gray_img) :
+	gray_img_filtered = cv2.bilateralFilter(gray_img, -1, 20, 20)
+	threshold_from_otsu, bin_tozero_img = cv2.threshold(gray_img_filtered, 0, 255, cv2.THRESH_TOZERO + cv2.THRESH_OTSU)
+	return cv2.adaptiveThreshold(bin_tozero_img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 3, 4)
+
+def Method2(gray_img) :
+	gray_img_filtered = cv2.bilateralFilter(gray_img, -1, 20, 20)
+	threshold_from_otsu, bin_tozero_img = cv2.threshold(gray_img_filtered, 0, 255, cv2.THRESH_TOZERO + cv2.THRESH_OTSU)
+	return cv2.Canny(gray_img_filtered, threshold_from_otsu / 2, threshold_from_otsu)
+
+def Method3(gray_img) :
+	gray_img_filtered = cv2.bilateralFilter(gray_img, -1, 20, 20)
+	return cv2.Laplacian(gray_img_filtered, -1)
+
+def PlateDetection(imagepath : str, Method, progress) :
+	img = cv2.imread(imagepath)
+	progress['value'] = 10
+	result = Method(cv2.cvtColor(img, cv2.COLOR_RGB2GRAY))
+	path_list = ObjectFinder(result, img)
+	progress['value'] = 40
+	for path in path_list :
+		plate_number = TextDetection(path)
+		progress['value'] += 30 / len(path_list)
+		if len(plate_number) != 0 :
+			return plate_number
+	return ""
+
